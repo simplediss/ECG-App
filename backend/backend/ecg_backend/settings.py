0839e6877bd5e1a636fbeceab275d95724908985
Lina Petrovsky <linapetrovsky@gmail.com>
2025-03-29 09:35:50 +0000

    change backend to israel timezone

--- backend/backend/ecg_backend/settings.py
+++ backend/backend/ecg_backend/settings.py
@@ -129,11 +129,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
+TIME_ZONE = 'Asia/Jerusalem'
+USE_I18N = False
 USE_TZ = True
 
 
