43df0217060923feec6ed18e31bc6885e005ef9f
Lina Petrovsky <linapetrovsky@gmail.com>
2025-03-29 22:20:45 +0300

    Fixed the navbar view on mobile + added icons

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -75,8 +75,15 @@ const Register = () => {
       <div className="auth-card">
         <div className="auth-header">
           <div className="auth-logo">
-            <span className="logo-icon">❤️</span>
-            <h1>ECG Master</h1>
+            <div className="logo-container">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="logo-icon activity-icon" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2"/>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="logo-icon heart-icon" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+              </svg>
+            </div>
+            <h1>ECGenius</h1>
           </div>
           <h2>Create Account</h2>
           <p className="auth-subtitle">Fill in your details to get started</p>
