75c35aab9a4340c605ea8eddd3fdf88000f6f936
Lina Petrovsky <linapetrovsky@gmail.com>
2025-03-29 10:04:46 +0000

    Deploy backend with WSGI using Gunicorn

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -8,7 +8,7 @@ echo "Populating ECG data..."
 python manage.py populate_ecg_data --snomed_csv=$DATASET_SNOMED_PATH --samples_dir=$DATASET_SAMPLES_PATH
 
 echo "Creating users..."
-python manage.py create_user --username --superuser $ADMIN_USERNAME --password $ADMIN_PASSWORD --email admin@example.com --first_name admin --last_name admin
+python manage.py create_user --superuser --username $ADMIN_USERNAME --password $ADMIN_PASSWORD --email admin@example.com --first_name admin --last_name admin
 
-echo "Starting backend server..."
-python manage.py runserver 0.0.0.0:8000
+echo "Starting backend server with Gunicorn..."
+gunicorn -c ../gunicorn_config.py ecg_backend.wsgi:application
